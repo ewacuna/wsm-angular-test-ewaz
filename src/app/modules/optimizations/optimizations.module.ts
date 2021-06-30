1d86072bacf67f41e507d68a37b5818c42892d68
Elvin Acuña <ewacunaz@gmail.com>
2021-06-30 11:59:23 -0600

    Add Campaign Variants

--- src/app/modules/optimizations/optimizations.module.ts
+++ src/app/modules/optimizations/optimizations.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { OptimizationsRoutingModule } from './optimizations-routing.module';
+
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 import { CampaignsComponent } from './pages';
 import { CampaignHeaderComponent } from './components';
 
 @NgModule({
   declarations: [CampaignsComponent, CampaignHeaderComponent],
-  imports: [CommonModule, OptimizationsRoutingModule],
+  imports: [CommonModule, OptimizationsRoutingModule, TabsModule.forRoot()],
 })
 export class OptimizationsModule {}
--- src/app/modules/optimizations/pages/campaigns/campaigns.component.ts
+++ src/app/modules/optimizations/pages/campaigns/campaigns.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ApiService } from 'src/app/core/services';
+import { SubscriptionsContainer } from 'src/app/shared/helpers';
 
 @Component({
   selector: 'app-campaigns',
   templateUrl: './campaigns.component.html',
-  styleUrls: ['./campaigns.component.scss']
+  styleUrls: ['./campaigns.component.scss'],
 })
-export class CampaignsComponent implements OnInit {
+export class CampaignsComponent implements OnInit, OnDestroy {
+  campaignData: Array<any> = [];
+  subs = new SubscriptionsContainer();
 
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
+    this.getCampaignData();
   }
 
+  ngOnDestroy(): void {
+    this.subs.dispose();
+  }
+
+  private getCampaignData() {
+    const endpoint: string =
+      'https://7e1ec65d-6cc3-4a1e-a781-c265f6cc45da.mock.pstmn.io/items/';
+    this.subs.add = this.apiService.sendGet(endpoint).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.campaignData = res;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
