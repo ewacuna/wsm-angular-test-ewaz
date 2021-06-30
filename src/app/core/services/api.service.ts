78cb5a13566bdda96b796ceda5d743486a275007
Elvin Acuña <ewacunaz@gmail.com>
2021-06-29 21:01:50 -0600

    Initial Commit

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  sendGet(endpoint: string) {
+    return this.http.get(endpoint);
+  }
+}
