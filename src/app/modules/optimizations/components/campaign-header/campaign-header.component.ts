1daeecc6228cf468cd370ab57d5878c476ac375b
Elvin Acuña <ewacunaz@gmail.com>
2021-06-30 12:35:41 -0600

    Update Campaign Header

--- src/app/modules/optimizations/components/campaign-header/campaign-header.component.ts
+++ src/app/modules/optimizations/components/campaign-header/campaign-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-campaign-header',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./campaign-header.component.scss'],
 })
 export class CampaignHeaderComponent implements OnInit {
+  @Input() campaignName: any;
+  @Input() campaignGroupName: any;
+  @Input() campaignLength: any;
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/modules/optimizations/pages/campaigns/campaigns.component.ts
+++ src/app/modules/optimizations/pages/campaigns/campaigns.component.ts
@@ -9,25 +9,57 @@ import { SubscriptionsContainer } from 'src/app/shared/helpers';
 })
 export class CampaignsComponent implements OnInit, OnDestroy {
   campaignData: Array<any> = [];
+  campaignName: string = '';
+  campaignLength: number = 0;
+  campaignGroupName: string = '';
   subs = new SubscriptionsContainer();
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.getCampaignData();
+    this.getCampaign();
+    this.getCampaignInfo();
+    this.getCampaignGroup();
   }
 
   ngOnDestroy(): void {
     this.subs.dispose();
   }
 
-  private getCampaignData() {
+  private getCampaign() {
     const endpoint: string =
       'https://7e1ec65d-6cc3-4a1e-a781-c265f6cc45da.mock.pstmn.io/items/';
     this.subs.add = this.apiService.sendGet(endpoint).subscribe(
       (res: any) => {
         console.log(res);
         this.campaignData = res;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  private getCampaignGroup() {
+    const endpoint: string =
+      'https://7e1ec65d-6cc3-4a1e-a781-c265f6cc45da.mock.pstmn.io/campaign-groups/?id=1';
+    this.subs.add = this.apiService.sendGet(endpoint).subscribe(
+      (res: any) => {
+        this.campaignGroupName = res[0]?.name || 'Unknown';
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  private getCampaignInfo() {
+    const endpoint: string =
+      'https://7e1ec65d-6cc3-4a1e-a781-c265f6cc45da.mock.pstmn.io/campaigns/';
+    this.subs.add = this.apiService.sendGet(endpoint).subscribe(
+      (res: any) => {
+        this.campaignName = res[0]?.name || 'Unknown';
+        this.campaignLength = res?.length || 0;
       },
       (error) => {
         console.log(error);
