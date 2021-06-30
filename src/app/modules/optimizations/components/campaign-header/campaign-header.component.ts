766f8cb91f1271b13a440a676f7bdcdec06d4929
Elvin Acuña <ewacunaz@gmail.com>
2021-06-30 10:19:49 -0600

    Update Campaign View

---
+++ src/app/modules/optimizations/components/campaign-header/campaign-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-campaign-header',
+  templateUrl: './campaign-header.component.html',
+  styleUrls: ['./campaign-header.component.scss'],
+})
+export class CampaignHeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/modules/optimizations/optimizations.module.ts
+++ src/app/modules/optimizations/optimizations.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { OptimizationsRoutingModule } from './optimizations-routing.module';
 import { CampaignsComponent } from './pages';
+import { CampaignHeaderComponent } from './components';
 
 @NgModule({
-  declarations: [CampaignsComponent],
+  declarations: [CampaignsComponent, CampaignHeaderComponent],
   imports: [CommonModule, OptimizationsRoutingModule],
 })
 export class OptimizationsModule {}
