c084fa5f57656a66b5b3c0903b900310b9ff5ebf
Elvin Acuña <ewacunaz@gmail.com>
2021-06-30 14:31:22 -0600

    Update Optimizations View

--- src/app/modules/optimizations/optimizations.module.ts
+++ src/app/modules/optimizations/optimizations.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import { OptimizationsRoutingModule } from './optimizations-routing.module';
 
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 import { CampaignsComponent } from './pages';
 import { CampaignHeaderComponent } from './components';
 
 @NgModule({
   declarations: [CampaignsComponent, CampaignHeaderComponent],
-  imports: [CommonModule, OptimizationsRoutingModule, TabsModule.forRoot()],
+  imports: [
+    CommonModule,
+    OptimizationsRoutingModule,
+    TabsModule.forRoot(),
+    AccordionModule.forRoot(),
+  ],
 })
 export class OptimizationsModule {}
